0be65664330094c43eec857252ecd58a26260581
github wubuchako <jfdev12@gmail.com>
2020-09-14 11:49:44 +0900

    add DOM

--- app.js
+++ app.js
@@ -17,4 +17,23 @@ activePlayer = 0;
 dice = Math.floor(Math.random() *6) + 1;
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice; //textContentは単にテキスト変更するだけ
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>'  + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
+
+
+
